59b396f05932bb97601f06d30e09d04f5729f526
Kuchenko Dmitriy <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-01 16:22:03 +0300

    Add express backend and admin registration

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import Header from '../../components/adminPannel/header/index'
-import Auth from '../../components/adminPannel/auth/index'
-import style from './style.module.css'
-import IsAuth from '../../components/adminPannel/isAuth'
+import React from 'react';
+import Header from '../../components/adminPannel/header/index';
+import Auth from '../../components/adminPannel/auth/index';
+import Register from '../../components/adminPannel/register/index';
+import style from './style.module.css';
+import IsAuth from '../../components/adminPannel/isAuth';
 import Loading  from "../../components/adminPannel/loading/loading";
-import { selectIsAuth, fetchAuthMe } from '../../redux/slices/auth'
-import { useSelector, useDispatch } from "react-redux"
+import { selectIsAuth, fetchAuthMe } from '../../redux/slices/auth';
+import { useSelector, useDispatch } from "react-redux";
+import axios from '../../axios/axios';
 
 function AdminPanel() {
-    const dispatch = useDispatch()
-    const isAuth = useSelector(selectIsAuth)
-    const { status } = useSelector((state)=> state.auth)
-    const isLoading = status === 'loading'
+    const dispatch = useDispatch();
+    const isAuth = useSelector(selectIsAuth);
+    const { status } = useSelector((state)=> state.auth);
+    const isLoading = status === 'loading';
+    const [exists, setExists] = React.useState(true);
+
     React.useEffect(() => {
-        dispatch(fetchAuthMe() )
-    }, [])
+        dispatch(fetchAuthMe());
+        axios.get('/admin-exists').then(res => setExists(res.data.exists));
+    }, []);
     return (
         <div className={style.backgound}>
             <div className={style.layout}> 
                 <Header />
-               {isLoading? <Loading/> : (isAuth? <IsAuth /> : <Auth/>)}  
+               {isLoading ? (
+                   <Loading />
+               ) : isAuth ? (
+                   <IsAuth />
+               ) : exists ? (
+                   <Auth />
+               ) : (
+                   <Register />
+               )}
             </div>
         </div>
     )
