4bfaca37836dca46b0292f25c065286a1adadf54
Kuchenko Dmitriy <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-01 18:14:19 +0300

    fix: use configurable API root for uploads (#16)

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api'
+  baseURL: API_URL
 });
 
 instance.interceptors.request.use((config) => {
@@ -9,4 +10,5 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
+export const API_ROOT = API_URL.replace(/\/api$/, '');
 export default instance;
