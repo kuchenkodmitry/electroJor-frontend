604d18229303e28b9150642bd25eb62dac0cc9ed
DmitriyKuchenko <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-02 03:05:10 +0300

    axios upd

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
-// Support both with and without the `/api` suffix in the environment variable.
-// `API_URL` should always point to the `/api` prefix while `API_ROOT` is the
-// host root used for serving static assets like uploaded images.
 const RAW_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+
 const API_URL = RAW_URL.endsWith('/api')
   ? RAW_URL
   : `${RAW_URL.replace(/\/$/, '')}/api`;
+
 const API_ROOT = API_URL.replace(/\/api$/, '');
 
 const instance = axios.create({
-  baseURL: API_URL
+  baseURL: API_URL,
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = window.localStorage.getItem('token');
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = token;
   return config;
 });
 
