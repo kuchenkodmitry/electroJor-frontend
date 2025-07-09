cfe897dfa475fac356427ba677ca0427b7b86344
Kuchenko Dmitriy <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-09 21:15:34 +0300

    feat: allow admin to change password (#26)

---
+++ src/components/adminPannel/password/edit.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changePassword } from "../../../redux/slices/auth";
+import { TextField, Button, Typography } from "@mui/material";
+import style from "./style.module.css";
+
+function EditPassword() {
+  const dispatch = useDispatch();
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const onSave = async () => {
+    const { error } = await dispatch(changePassword({ oldPassword, newPassword }));
+    if (!error) {
+      setOldPassword('');
+      setNewPassword('');
+      alert('Пароль обновлён');
+    } else {
+      alert('Не удалось изменить пароль');
+    }
+  };
+
+  return (
+    <div>
+      <Typography
+        sx={{
+          fontFamily: "SourceCodePro-SemiBold",
+          fontSize: "24px",
+          textAlign: "center",
+          marginTop: "27px",
+          marginBottom: "20px",
+        }}
+      >
+        Сменить пароль
+      </Typography>
+      <TextField
+        className={style.input}
+        value={oldPassword}
+        onChange={(e) => setOldPassword(e.target.value)}
+        label="Старый пароль"
+        type="password"
+        variant="outlined"
+      />
+      <TextField
+        className={style.input}
+        value={newPassword}
+        onChange={(e) => setNewPassword(e.target.value)}
+        label="Новый пароль"
+        type="password"
+        variant="outlined"
+      />
+      <Button variant="contained" onClick={onSave}>
+        Сохранить
+      </Button>
+    </div>
+  );
+}
+
+export default EditPassword;
