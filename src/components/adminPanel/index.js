25c11b462afa87761b91e0f8ac105a1ee7d498df
Kuchenko Dmitriy <161463592+kuchenkodmitry@users.noreply.github.com>
2025-07-24 03:57:19 +0300

    feat: add mobile menu button

--- src/components/adminPanel/index.js
+++ src/components/adminPanel/index.js
@@ -3,3 +3,4 @@ export { default as Auth } from './Auth';
 export { default as Register } from './Register';
 export { default as AdminLayout } from './AdminLayout';
 export { default as Loading } from './Loading/loading';
+export { default as MobileMenuButton } from './MobileMenuButton';
